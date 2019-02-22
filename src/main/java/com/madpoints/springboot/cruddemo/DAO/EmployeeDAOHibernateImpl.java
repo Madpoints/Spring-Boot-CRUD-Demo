513ae95bb881941de4647efe2059c3960150bc62
John Furtado <jf8846@gmail.com>
2019-02-21 19:10:04 -0500

    implement EntityManager using contructor injection

--- src/main/java/com/madpoints/springboot/cruddemo/DAO/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/madpoints/springboot/cruddemo/DAO/EmployeeDAOHibernateImpl.java
@@ -2,12 +2,22 @@ package com.madpoints.springboot.cruddemo.DAO;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.madpoints.springboot.cruddemo.entity.Employee;
 
 @Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	private EntityManager entityManager;
+	
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public List<Employee> findAll() {
