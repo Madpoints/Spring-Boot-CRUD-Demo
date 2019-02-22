ab64506a50a376181944b2b3bbe1e6c45c4ffe37
John Furtado <jf8846@gmail.com>
2019-02-22 16:55:59 -0500

    delete mapping deletes a employee from the db

--- src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.madpoints.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,4 +62,18 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.findById(employeeId);
+		
+		if (tempEmployee == null) {
+			
+			throw new RuntimeException("Employee id not found: " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee with id: " + employeeId;
+	}
 }
