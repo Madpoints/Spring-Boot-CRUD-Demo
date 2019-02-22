eb9b308d4106c816505aa7edf1f00ee9635b5379
John Furtado <jf8846@gmail.com>
2019-02-22 13:41:55 -0500

    additional CRUD methods and removed Transactional annotation

--- src/main/java/com/madpoints/springboot/cruddemo/DAO/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/madpoints/springboot/cruddemo/DAO/EmployeeDAOHibernateImpl.java
@@ -8,7 +8,6 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.madpoints.springboot.cruddemo.entity.Employee;
 
@@ -23,7 +22,6 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	}
 
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
 		
 		Session currentSession = entityManager.unwrap(Session.class);
@@ -34,6 +32,39 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 		List<Employee> employees = theQuery.getResultList();
 		
 		return employees;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		Employee theEmployee =
+				currentSession.get(Employee.class, theId);
+		
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void deleteById(int theId) {
+		
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		Query theQuery =
+				currentSession.createQuery(
+						"delete from Employee where id=:employeeId");
+		theQuery.setParameter("employeeId", theId);
+		
+		theQuery.executeUpdate();
 	}
 	
 }
