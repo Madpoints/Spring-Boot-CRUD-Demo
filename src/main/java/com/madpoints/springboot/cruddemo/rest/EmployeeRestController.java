8f27cced5544d1552a5e041d927b281b5cdbc603
John Furtado <jf8846@gmail.com>
2019-02-21 19:25:19 -0500

    /employees endpoint returns a list employees

--- src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,9 +1,13 @@
 package com.madpoints.springboot.cruddemo.rest;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.madpoints.springboot.cruddemo.DAO.EmployeeDAO;
+import com.madpoints.springboot.cruddemo.entity.Employee;
 
 @RestController
 @RequestMapping("/api")
@@ -12,7 +16,13 @@ public class EmployeeRestController {
 	private EmployeeDAO employeeDAO;
 
 	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+		
 		employeeDAO = theEmployeeDAO;
 	}
 	
+	@GetMapping("/employees")
+	public List<Employee> findAll() {
+		
+		return employeeDAO.findAll();
+	}
 }
