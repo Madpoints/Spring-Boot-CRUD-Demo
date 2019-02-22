ddaf23530ea3cb2d665814dbad477e720ab5af82
John Furtado <jf8846@gmail.com>
2019-02-21 19:22:02 -0500

    inject employee DAO using constructor injection

--- src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,11 +3,16 @@ package com.madpoints.springboot.cruddemo.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.madpoints.springboot.cruddemo.DAO.EmployeeDAO;
+
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-	
-	
+	private EmployeeDAO employeeDAO;
+
+	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
 	
 }
