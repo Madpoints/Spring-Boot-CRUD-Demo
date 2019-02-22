0e92ab9441914591f896c64fed3bb858045b652b
John Furtado <jf8846@gmail.com>
2019-02-22 16:13:20 -0500

    get mapping to find a employee by id

--- src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/madpoints/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,18 @@ public class EmployeeRestController {
 	public List<Employee> findAll() {
 		return employeeService.findAll();
 	}
+	
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if (theEmployee == null) {
+			
+			throw new RuntimeException("Employee id not found: " + employeeId);
+		}
+		
+		return theEmployee;
+	}
+	
 }
